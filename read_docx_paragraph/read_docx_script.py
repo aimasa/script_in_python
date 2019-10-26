93093133fdea79503816b70fbcf46d207f7ced63
aimasa <794182811@qq.com>
2019-10-26 19:25:43 +0800

    添加容错处理

--- read_docx_paragraph/read_docx_script.py
+++ read_docx_paragraph/read_docx_script.py
@@ -35,26 +35,39 @@ def read_docx(contract_file_path,label_file_name_header):
 
 # 将对应条款内容存储进入对应的标签中
 def save(label_file_name,para):
-    if not os.path.exists(label_file_name):
-        os.makedirs(label_file_name)
+    check_is_exit(label_file_name)
     with open(label_file_name,'a', encoding= 'UTF-8') as f:
         f.write(para)
         f.write("\n\n")
 
+# 检查文件夹是否存在，不存在则创建
+def check_is_exit(label_file_name):
+    if not os.path.exists(label_file_name):
+        os.makedirs(label_file_name)
+
 # 读取文件夹中的文件数据
 def read_dir_file(dir_file_path):
-    contract_file_path = os.listdir(dir_file_path)
+    try:
+        contract_file_path = os.listdir(dir_file_path)
+    except:
+        print("文档位置异常，请检查")
     return contract_file_path
 
+# 把已经处理完的文件转移到另一个文件夹中
 def move_file_to_other_dir(contract_file_path,new_dir):
+    check_is_exit(new_dir)
     shutil.move(contract_file_path,new_dir)
 
 if __name__ == "__main__":
+    # 待处理合同文件的文件夹位置
     dir_file_path = "F:/毕业设计涉及论文/合同文本数据"
+    # 标签文件存放位置
     label_file_name_header = "G:/tim照片消息缓存聚集地/消息缓存/794182811/FileRecv/real2"
+    # 处理完成的合同文件转移存放位置
     new_dir = "F:/毕业设计涉及论文/temp"
     for contract_file_path in read_dir_file(dir_file_path):
         contract_file_path = dir_file_path + "/" + contract_file_path
+        # 进入下一个合同文件处理
         print('\033[0;31;40m\t'+contract_file_path+'\033[0m')
         read_docx(contract_file_path,label_file_name_header)
         move_file_to_other_dir(contract_file_path,new_dir)
